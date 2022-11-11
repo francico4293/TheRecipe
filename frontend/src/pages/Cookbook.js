958ade6461e1010096e36b86a8955718824a2cc9
Colin Francis <colinfrancis@Colins-MacBook-Pro.local>
2022-11-11 18:00:38 -0500

    Added some initial content sectioning to cookbook page

--- frontend/src/pages/Cookbook.js
+++ frontend/src/pages/Cookbook.js
@@ -9,14 +9,19 @@ const Cookbook = () => {
     const user = useSelector(state => state.user);
 
     return (
-        <div id='cookbook-container'>
+        <>
             <Header/>
-            <IngredientDefinitionTool />
-            <div className='cookbook-results'>
-                {user.data.recipes.map((recipe, idx) => <RecipeResult result={recipe} page={'cookbook'} key={idx}/>)}
+            <div id='cookbook-profile-container'>
+                <h1>Welcome to your Cookbook, {user.data.firstName}.</h1>
+            </div>
+            <div id='cookbook-results-container'>
+                <IngredientDefinitionTool />
+                <div id='cookbook-results'>
+                    {user.data.recipes.map((recipe, idx) => <RecipeResult result={recipe} page={'cookbook'} key={idx}/>)}
+                </div>
             </div>
             <Footer/>
-        </div>
+        </>
     );
 }
 
