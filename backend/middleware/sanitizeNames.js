a3503dfed9b45dbaa564574262a7163572d3165d
Colin Francis <colinfrancis@Colins-MacBook-Pro.local>
2022-11-22 14:16:48 -0500

    Added docstring to middleware function

--- backend/middleware/sanitizeNames.js
+++ backend/middleware/sanitizeNames.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/**
+ * Puts the first name and last name in the correct format, i.e., first letter is capitalized.
+ * @param {object} req - The request body.
+ * @param {object} res - The response object. 
+ * @param {function} next - A function used to progress to the next piece of middleware. 
+ */
 const sanitizeNames = (req, res, next) => {
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
